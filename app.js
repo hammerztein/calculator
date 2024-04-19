bc1233f41c2883321620bacea03ee3e3cf2110f8
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-04-19 09:07:52 +0300

    Add a function to call correct math operation based on input operator. Specify different return statement when user tries to divide by zero.

--- app.js
+++ app.js
@@ -22,3 +22,36 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1 / num2;
 }
+
+// Call correct math operation
+function operate(num1, num2, operator) {
+	const firstNumber = +num1;
+	const secondNumber = +num2;
+	let result = 0;
+	// TODO make sure that numbers are not empty
+
+	switch (operator) {
+		case '+':
+			result = add(firstNumber, secondNumber);
+			break;
+		case '-':
+			result = subtract(firstNumber, secondNumber);
+			break;
+		case '*':
+			result = multiply(firstNumber, secondNumber);
+			break;
+		case '/':
+			if (secondNumber === 0) result = "Hey Brainiac you can't divide by zero.";
+			result = divide(firstNumber, secondNumber);
+			break;
+		default:
+			result = 'Please choose correct operator!';
+	}
+
+	// Reset operators and operand
+	firstOperand = '';
+	secondOperand = '';
+	operatorValue = '';
+
+	return result;
+}
