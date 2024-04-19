986b167d818e4c607dff2c484e1ff39a65a81772
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-04-19 07:35:21 +0300

    Store button container in a variable to acces all children. Create variables to store operands and operator values. Create 4 basic math operator functions.

---
+++ app.js
@@ -0,0 +1,24 @@
+// HTML DOM elements
+const buttons = document.querySelector('.buttons');
+
+// Calculator variables
+let firstOperand = '';
+let secondOperand = '';
+let operatorValue = '';
+
+// Math operator funcionts
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
